f1021f726552a1de3fae8a6e70b79104b92adadd
VoidmatrixHeathcliff <46210663+VoidmatrixHeathcliff@users.noreply.github.com>
2021-05-15 21:41:55 +0800

    Adding Module XML

--- src/ModuleXML.cpp
+++ src/ModuleXML.cpp
@@ -1,5 +1,19 @@
 #include "ModuleXML.h"
 
+
+unordered_map<int, int> ModuleXML::mapNodeType = {
+	{ node_null, NODETYPE_EMPTY },
+	{ node_document, NODETYPE_DOCUMENT },
+	{ node_element, NODETYPE_ELEMENT },
+	{ node_pcdata, NODETYPE_PLAINCHARADATA },
+	{ node_cdata, NODETYPE_CHARADATA },
+	{ node_comment, NODETYPE_COMMENT },
+	{ node_pi, NODETYPE_PROCESSINS },
+	{ node_declaration, NODETYPE_DECLARATION },
+	{ node_doctype, NODETYPE_DOCTYPE },
+};
+
+
 ModuleXML& ModuleXML::Instance()
 {
 	static ModuleXML* _instance = new ModuleXML();
@@ -14,18 +28,304 @@ ModuleXML::ModuleXML()
 		{ "LoadXMLFromFile", loadXMLFromFile },
 		{ "LoadXMLFromData", loadXMLFromData },
 	};
+
+	_vMetaData = {
+		{
+			METANAME_DOCUMENT,
+			{
+				{ "GetChild", document_GetChild },
+			},
+			__gc_Document
+		},
+		{
+			METANAME_NODE,
+			{
+				{ "node_GetRoot", node_GetRoot },
+				{ "node_GetParent", node_GetParent },
+				{ "node_GetNextSibling", node_GetNextSibling },
+				{ "node_GetPreviousSibling", node_GetPreviousSibling },
+				{ "node_GetChild", node_GetChild },
+				{ "node_GetFirstChild", node_GetFirstChild },
+				{ "node_GetLastChild", node_GetLastChild },
+				{ "node_GetName", node_GetName },
+				{ "node_GetValue", node_GetValue },
+				{ "node_GetChildValue", node_GetChildValue },
+				{ "node_GetType", node_GetType },
+				{ "node_GetText", node_GetText },
+				{ "node_GetTextAsInteger", node_GetTextAsInteger },
+				{ "node_GetTextAsNumber", node_GetTextAsNumber },
+				{ "node_GetAttribute", node_GetAttribute },
+				{ "node_GetFirstAttribute", node_GetFirstAttribute },
+				{ "node_GetLastAttribute", node_GetLastAttribute },
+			},
+			__gc_Node
+		},
+	};
+}
+
+
+ETHER_API node_GetRoot(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	CopyAndPushNewUserdataToStack(L, xml_node,
+		node->parent(), METANAME_NODE);
+
+	return 1;
+}
+
+
+ETHER_API node_GetParent(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	CopyAndPushNewUserdataToStack(L, xml_node,
+		node->parent(), METANAME_NODE);
+
+	return 1;
+}
+
+
+ETHER_API node_GetNextSibling(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	CopyAndPushNewUserdataToStack(L, xml_node,
+		node->next_sibling(), METANAME_NODE);
+
+	return 1;
+}
+
+
+ETHER_API node_GetPreviousSibling(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	CopyAndPushNewUserdataToStack(L, xml_node,
+		node->previous_sibling(), METANAME_NODE);
+
+	return 1;
+}
+
+
+ETHER_API node_GetChild(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	CopyAndPushNewUserdataToStack(L, xml_node, 
+		node->child(luaL_checkstring(L, 2)), METANAME_NODE);
+
+	return 1;
+}
+
+
+ETHER_API node_GetFirstChild(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	CopyAndPushNewUserdataToStack(L, xml_node,
+		node->first_child(), METANAME_NODE);
+
+	return 1;
+}
+
+
+ETHER_API node_GetLastChild(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	CopyAndPushNewUserdataToStack(L, xml_node,
+		node->last_child(), METANAME_NODE);
+
+	return 1;
+}
+
+
+ETHER_API node_GetName(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	lua_pushstring(L, node->name());
+
+	return 1;
+}
+
+
+ETHER_API node_GetValue(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	lua_pushstring(L, node->value());
+
+	return 1;
+}
+
+
+ETHER_API node_GetChildValue(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	lua_pushstring(L, lua_gettop(L) > 1 ? 
+		node->child_value(luaL_checkstring(L, 2)) : node->child_value());
+
+	return 1;
+}
+
+
+ETHER_API node_GetType(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	auto itorType = ModuleXML::mapNodeType.find(node->type());
+	if (itorType != ModuleXML::mapNodeType.end())
+		lua_pushinteger(L, itorType->second);
+	else
+		lua_pushinteger(L, NODETYPE_EMPTY);
+
+	return 1;
+}
+
+
+ETHER_API node_GetText(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	lua_pushstring(L, node->text().as_string());
+
+	return 1;
+}
+
+
+ETHER_API node_GetTextAsInteger(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	lua_pushinteger(L, node->text().as_llong());
+
+	return 1;
+}
+
+
+ETHER_API node_GetTextAsNumber(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	lua_pushnumber(L, node->text().as_double());
+
+	return 1;
+}
+
+
+ETHER_API node_GetAttribute(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	CopyAndPushNewUserdataToStack(L, xml_attribute,
+		node->attribute(luaL_checkstring(L, 2)), METANAME_ATTRIBUTE);
+
+	return 1;
+}
+
+
+ETHER_API node_GetFirstAttribute(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	CopyAndPushNewUserdataToStack(L, xml_attribute,
+		node->first_attribute(), METANAME_ATTRIBUTE);
+
+	return 1;
+}
+
+
+ETHER_API node_GetLastAttribute(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	CopyAndPushNewUserdataToStack(L, xml_attribute,
+		node->last_attribute(), METANAME_ATTRIBUTE);
+
+	return 1;
+}
+
+
+ETHER_API __gc_Node(lua_State* L)
+{
+	xml_node* node = GetNodeDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckNodeDataAt1stPos(node);
+#endif
+	delete node;
+	node = nullptr;
+
+	return 0;
+}
+
+
+ETHER_API document_GetChild(lua_State* L)
+{
+	xml_document* doc = GetDocumentDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckDocumentDataAt1stPos(doc);
+#endif
+	CopyAndPushNewUserdataToStack(L, xml_node,
+		doc->child(luaL_checkstring(L, 2)), METANAME_NODE);
+
+	return 1;
+}
+
+
+ETHER_API __gc_Document(lua_State* L)
+{
+	xml_document* doc = GetDocumentDataAt1stPos();
+#ifdef _ETHER_DEBUG_
+	CheckDocumentDataAt1stPos(doc);
+#endif
+	delete doc;
+	doc = nullptr;
+
+	return 0;
 }
 
 
 ETHER_API loadXML(lua_State* L)
 {
-	xml_document* doc = new xml_document();
-	xml_parse_result result = doc->load_string(luaL_checkstring(L, 1));
-	luaL_argcheck(L, !result.status, 1, string("XML parse error: ").append(result.description()).c_str());
-	xml_document** uppdoc = (xml_document**)lua_newuserdata(L, sizeof(xml_document*));
-	*uppdoc = doc;
-	luaL_getmetatable(L, METANAME_DOCUMENT);
-	lua_setmetatable(L, -2);
+	xml_document* document = new xml_document();
+	LoadXMLAndCheck(document, document->load_string(luaL_checkstring(L, 1)));
 
 	return 1;
 }
@@ -33,13 +333,8 @@ ETHER_API loadXML(lua_State* L)
 
 ETHER_API loadXMLFromFile(lua_State* L)
 {
-	xml_document* doc = new xml_document();
-	xml_parse_result result = doc->load_file(luaL_checkstring(L, 1));
-	luaL_argcheck(L, !result.status, 1, string("XML parse error: ").append(result.description()).c_str());
-	xml_document** uppdoc = (xml_document**)lua_newuserdata(L, sizeof(xml_document*));
-	*uppdoc = doc;
-	luaL_getmetatable(L, METANAME_DOCUMENT);
-	lua_setmetatable(L, -2);
+	xml_document* document = new xml_document();
+	LoadXMLAndCheck(document, document->load_file(luaL_checkstring(L, 1)));
 
 	return 1;
 }
@@ -48,13 +343,8 @@ ETHER_API loadXMLFromFile(lua_State* L)
 ETHER_API loadXMLFromData(lua_State* L)
 {
 	size_t len;
-	xml_document* doc = new xml_document();
-	xml_parse_result result = doc->load_buffer(luaL_checklstring(L, 1, &len), len);
-	luaL_argcheck(L, !result.status, 1, string("XML parse error: ").append(result.description()).c_str());
-	xml_document** uppdoc = (xml_document**)lua_newuserdata(L, sizeof(xml_document*));
-	*uppdoc = doc;
-	luaL_getmetatable(L, METANAME_DOCUMENT);
-	lua_setmetatable(L, -2);
+	xml_document* document = new xml_document();
+	LoadXMLAndCheck(document, document->load_buffer(luaL_checklstring(L, 1, &len), len));
 
 	return 1;
 }
--- src/ModuleXML.h
+++ src/ModuleXML.h
@@ -8,11 +8,47 @@
 #include <pugixml.hpp>
 using namespace pugi;
 
-#define METANAME_DOCUMENT	"XML.Document"
+#include <unordered_map>
+using namespace std;
+
+#define NODETYPE_EMPTY						0
+#define NODETYPE_DOCUMENT					1
+#define NODETYPE_ELEMENT					2
+#define NODETYPE_PLAINCHARADATA				3
+#define NODETYPE_CHARADATA					4
+#define NODETYPE_COMMENT					5
+#define NODETYPE_PROCESSINS					6
+#define NODETYPE_DECLARATION				7
+#define NODETYPE_DOCTYPE					8
+
+#define METANAME_DOCUMENT					"XML.Document"
+#define METANAME_NODE						"XML.Node"
+#define METANAME_ATTRIBUTE					"XML.Attribute"
+
+#define GetDocumentDataAt1stPos()			(xml_document*)(*(void**)luaL_checkudata(L, 1, METANAME_DOCUMENT))
+#define GetNodeDataAt1stPos()				(xml_node*)(*(void**)luaL_checkudata(L, 1, METANAME_NODE))
+#define GetAttributeDataAt1stPos()			(xml_attribute*)(*(void**)luaL_checkudata(L, 1, METANAME_ATTRIBUTE))
+
+#define CheckDocumentDataAt1stPos(doc)		luaL_argcheck(L, doc, 1, "get document data failed")
+#define CheckNodeDataAt1stPos(node)			luaL_argcheck(L, node, 1, "get node data failed")
+#define CheckAttributeDataAt1stPos(attri)	luaL_argcheck(L, attri, 1, "get attribute data failed")
+
+#define LoadXMLAndCheck(doc, res)\
+	xml_parse_result result = res;\
+	luaL_argcheck(L, !result.status, 1, string("XML parse error: ").append(result.description()).c_str());\
+	xml_document** uppdoc = (xml_document**)lua_newuserdata(L, sizeof(xml_document*));\
+	*uppdoc = doc; luaL_getmetatable(L, METANAME_DOCUMENT); lua_setmetatable(L, -2);
+
+#define CopyAndPushNewUserdataToStack(L, T, src, name)\
+	T* p = new T(src);\
+	if (p->empty()) lua_pushnil(L);\
+	else { T** upp = (T**)lua_newuserdata(L, sizeof(T*));\
+		*upp = p; luaL_getmetatable(L, name); lua_setmetatable(L, -2); }
 
 class ModuleXML : public Module
 {
 public:
+	static unordered_map<int, int> mapNodeType;
 	static ModuleXML& Instance();
 	~ModuleXML() {};
 
@@ -21,8 +57,50 @@ private:
 
 };
 
+ETHER_API node_GetRoot(lua_State* L);
+
+ETHER_API node_GetParent(lua_State* L);
+
+ETHER_API node_GetNextSibling(lua_State* L);
+
+ETHER_API node_GetPreviousSibling(lua_State* L);
+
+ETHER_API node_GetChild(lua_State* L);
+
+ETHER_API node_GetFirstChild(lua_State* L);
+
+ETHER_API node_GetLastChild(lua_State* L);
+
+ETHER_API node_GetName(lua_State* L);
+
+ETHER_API node_GetValue(lua_State* L);
+
+ETHER_API node_GetChildValue(lua_State* L);
+
+ETHER_API node_GetType(lua_State* L);
+
+ETHER_API node_GetText(lua_State* L);
+
+ETHER_API node_GetTextAsInteger(lua_State* L);
+
+ETHER_API node_GetTextAsNumber(lua_State* L);
+
+ETHER_API node_GetAttribute(lua_State* L);
+
+ETHER_API node_GetFirstAttribute(lua_State* L);
+
+ETHER_API node_GetLastAttribute(lua_State* L);
+
+ETHER_API __gc_Node(lua_State* L);
+
+ETHER_API document_GetChild(lua_State* L);
+
+ETHER_API __gc_Document(lua_State* L);
+
 ETHER_API loadXML(lua_State* L);
 
 ETHER_API loadXMLFromFile(lua_State* L);
+
+ETHER_API loadXMLFromData(lua_State* L);
 
 #endif // !_MODULE_XML_H_
