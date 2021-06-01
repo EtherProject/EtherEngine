ed8fbcada057142144b33ff5e0d90287a1791562
Fa鸽 <1207247008@qq.com>
2021-06-01 22:50:21 +0800

    移除的_ETHER_DEBUG_宏，交由生成系统处理

--- src/Macros.h
+++ src/Macros.h
@@ -2,7 +2,11 @@
 #define _MACROS_H_
 
 /* 定义 _ETHER_DEUBG_ 以开启Debug模式 */
-#define _ETHER_DEBUG_
+
+// 不用了，由生成系统代劳，下个版本删除
+// GCC添加参数 -D_ETHER_DEBUG
+
+// #define _ETHER_DEBUG_
 
 #define _VERSION_ "Alpha v4.1.0"
 
