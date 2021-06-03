73d0d8165f46ae12b2ff566f69d61f5a0638b06f
Fa鸽 <1207247008@qq.com>
2021-06-03 20:07:40 +0800

    字符串转换大概没问题了
    砍UTF32转换功能，因为不好做

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,8 +1,10 @@
-#include "Utils.h"
-
-#if _MSVC_LANG >= 201703L
-#include <stringapiset.h>
+#if _WIN32
+#define _WIN32_LEAN_AND_MEAN 1
+// 逃不掉的，只能把整个windows.h放进来
+#include <Windows.h>
 #endif
+
+#include "Utils.h"
 
 template<class Facet>
 struct deletable_facet : Facet
@@ -17,40 +19,139 @@ using mbs_facet_t = deletable_facet<std::codecvt_byname<wchar_t, char, std::mbst
 #if defined(_WIN32) || defined(_WIN64)
 const char* EncodingConversion::GBK_LOCALE_NAME = ".936";
 #else
-const char* EncodingConversion::GBK_LOCALE_NAME = "zh_CN.GBK";
+const char* EncodingConversion::GBK_LOCALE_NAME = "zh_CN.GB18030";
 #endif
 
 std::string EncodingConversion::ToString(const std::wstring& wstr)
 {
-	//std::locale::global(std::locale(""));
+#ifdef _WIN32
+	size_t wcs_size = wstr.size();
+
+	// ACP，默认代码页
+	int mbcs_size = WideCharToMultiByte(
+		CP_ACP,
+		0,
+		wstr.data(),
+		wcs_size >= INT_MAX ? throw std::invalid_argument("wstring is too long") : (int)wcs_size,
+		nullptr, 0, nullptr, nullptr
+	);
+
+	std::string mbcs{ (size_t)mbcs_size, '\0', std::allocator<char>{} };
+
+	WideCharToMultiByte(
+		CP_ACP,
+		0,
+		wstr.data(), (int)wcs_size,
+		mbcs.data(), mbcs_size,
+		nullptr, nullptr
+	);
+
+	return mbcs;
+#else // posix
+	// 考虑使用iconv
 	const mbs_facet_t& cvt = std::use_facet<mbs_facet_t>(std::locale());
 	std::wstring_convert<mbs_facet_t> converter(&cvt);
 	std::string str = converter.to_bytes(wstr);
 	return str;
-	return "";
+#endif
+
 }
 
 std::wstring EncodingConversion::ToWString(const std::string& str)
 {
+#ifdef _WIN32
+	size_t mbcs_size = str.size();
+
+	// ACP，默认代码页
+	int wcs_size = MultiByteToWideChar(
+		CP_ACP,
+		0,
+		str.data(),
+		mbcs_size >= INT_MAX ? throw std::invalid_argument("wstring is too long") : (int)mbcs_size,
+		nullptr, 0
+	);
+
+	std::wstring wcs{ (size_t)wcs_size, L'\0', std::allocator<wchar_t>{} };
+
+	MultiByteToWideChar(
+		CP_ACP,
+		0,
+		str.data(), (int)mbcs_size,
+		wcs.data(), wcs_size
+	);
+
+	return wcs;
+#else
 	//std::locale::global(std::locale(""));
 	const mbs_facet_t& cvt = std::use_facet<mbs_facet_t>(std::locale());
 	std::wstring_convert<mbs_facet_t> converter(&cvt);
 	std::wstring wstr = converter.from_bytes(str);
 	return wstr;
+#endif
 }
 
 std::string EncodingConversion::ToGBK(const std::wstring& wstr)
 {
+#ifdef _WIN32
+	size_t wcs_size = wstr.size();
+
+	// 936即GBK
+	int mbcs_size = WideCharToMultiByte(
+		936,
+		0,
+		wstr.data(),
+		wcs_size >= INT_MAX ? throw std::invalid_argument("wstring is too long") : (int)wcs_size,
+		nullptr, 0, nullptr, nullptr
+	);
+
+	std::string mbcs{ (size_t)mbcs_size, '\0', std::allocator<char>{} };
+
+	WideCharToMultiByte(
+		936,
+		0,
+		wstr.data(), (int)wcs_size,
+		mbcs.data(), mbcs_size,
+		nullptr, nullptr
+	);
+
+	return mbcs;
+#else
 	std::wstring_convert<mbs_facet_t> conv(new mbs_facet_t(GBK_LOCALE_NAME));
 	std::string  str = conv.to_bytes(wstr);
 	return str;
+#endif // _WIN32
+
 }
 
 std::wstring EncodingConversion::FromGBK(const std::string& str)
 {
+#ifdef _WIN32
+	size_t mbcs_size = str.size();
+
+	// ACP，默认代码页
+	int wcs_size = MultiByteToWideChar(
+		936,
+		0,
+		str.data(),
+		mbcs_size >= INT_MAX ? throw std::invalid_argument("wstring is too long") : (int)mbcs_size,
+		nullptr, 0
+	);
+
+	std::wstring wcs{ (size_t)wcs_size, L'\0', std::allocator<wchar_t>{} };
+
+	MultiByteToWideChar(
+		936,
+		0,
+		str.data(), (int)mbcs_size,
+		wcs.data(), wcs_size
+	);
+
+	return wcs;
+#else
 	std::wstring_convert<mbs_facet_t> conv(new mbs_facet_t(GBK_LOCALE_NAME));
 	std::wstring wstr = conv.from_bytes(str);
 	return wstr;
+#endif
 }
 
 std::string EncodingConversion::ToUTF8(const std::wstring& wstr)
@@ -61,19 +162,29 @@ std::string EncodingConversion::ToUTF8(const std::wstring& wstr)
 #elif defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__)
 	std::wstring_convert<std::codecvt_utf8<wchar_t>> convert;
 	return convert.to_bytes(wstr.data(), wstr.data() + wstr.size());
-#elif defined(_WIN32) || defined(_WIN64)
+#elif defined(_WIN32)
 	size_t wcs_size = wstr.size();
-	// 计算字符串长度
-	int u8_size = WideCharToMultiByte(
+
+	int mbcs_size = WideCharToMultiByte(
 		CP_UTF8,
 		0,
 		wstr.data(),
 		wcs_size >= INT_MAX ? throw std::invalid_argument("wstring is too long") : (int)wcs_size,
-		nullptr, 0,
-		'\0', nullptr
+		nullptr, 0, nullptr, nullptr
 	);
 
-	std::string ret{}
+	std::string mbcs{ (size_t)mbcs_size, '\0', std::allocator<char>{} };
+
+	WideCharToMultiByte(
+		CP_UTF8,
+		0,
+		wstr.data(), (int)wcs_size,
+		mbcs.data(), mbcs_size,
+		nullptr, nullptr
+	);
+
+	return mbcs;
+
 #endif
 }
 
@@ -86,9 +197,28 @@ std::wstring EncodingConversion::FromUTF8(const std::string& str)
 #elif defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__)
 	std::wstring_convert<std::codecvt_utf8<wchar_t>> convert;
 	return convert.from_bytes(str.data(), str.data() + str.size());
-#elif defined(_WIN32) || defined(_WIN64)
-	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
-	return convert.from_bytes(str.data(), str.data() + str.size());
+#elif defined(_WIN32)
+	size_t mbcs_size = str.size();
+
+	int wcs_size = MultiByteToWideChar(
+		CP_UTF8,
+		0,
+		str.data(),
+		mbcs_size >= INT_MAX ? throw std::invalid_argument("wstring is too long") : (int)mbcs_size,
+		nullptr, 0
+	);
+
+	std::wstring wcs{ (size_t)wcs_size, L'\0', std::allocator<wchar_t>{} };
+
+	// Windows Vista后等价
+	MultiByteToWideChar(
+		CP_UTF8,
+		0,
+		str.data(), (int)mbcs_size,
+		wcs.data(), wcs_size
+	);
+
+	return wcs;
 #endif
 }
 
@@ -104,10 +234,28 @@ std::string EncodingConversion::UTF8ToGBK(const std::string& str)
 
 std::u16string EncodingConversion::UTF8toUTF16(const std::string& str)
 {
-#if defined(_MSC_VER)
-	std::wstring_convert<std::codecvt_utf8_utf16<uint16_t>, uint16_t> convert;
-	auto tmp = convert.from_bytes(str.data(), str.data() + str.size());
-	return std::u16string(tmp.data(), tmp.data() + tmp.size());
+#ifdef _WIN32
+	size_t mbcs_size = str.size();
+
+	int wcs_size = MultiByteToWideChar(
+		CP_UTF8,
+		0,
+		str.data(),
+		mbcs_size >= INT_MAX ? throw std::invalid_argument("wstring is too long") : (int)mbcs_size,
+		nullptr, 0
+	);
+
+	std::u16string wcs{ (size_t)wcs_size, u'\0', std::allocator<char16_t>{} };
+	
+	// Windows Vista后等价
+	MultiByteToWideChar(
+		CP_UTF8,
+		0,
+		str.data(), (int)mbcs_size,
+		(wchar_t*)wcs.data(), wcs_size
+	);
+
+	return wcs;
 #else
 	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
 	return convert.from_bytes(str.data(), str.data() + str.size());
@@ -117,20 +265,37 @@ std::u16string EncodingConversion::UTF8toUTF16(const std::string& str)
 std::u32string EncodingConversion::UTF8toUTF32(const std::string& str)
 {
 #if defined(_MSC_VER)
-	std::wstring_convert<std::codecvt_utf8<uint32_t>, uint32_t> convert;
-	auto tmp = convert.from_bytes(str.data(), str.data() + str.size());
-	return std::u32string(tmp.data(), tmp.data() + tmp.size());
+	throw std::logic_error("utf32 conversion is not implemented");
 #else
 	std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> convert;
 	return convert.from_bytes(str.data(), str.data() + str.size());
 #endif
 }
 
-std::string EncodingConversion::UTF16toUTF8(const std::u16string& str)
+std::string EncodingConversion::UTF16toUTF8(const std::u16string& wstr)
 {
-#if defined(_MSC_VER)
-	std::wstring_convert<std::codecvt_utf8_utf16<uint16_t>, uint16_t> convert;
-	return convert.to_bytes((uint16_t*)str.data(), (uint16_t*)str.data() + str.size());
+#ifdef _WIN32
+	size_t wcs_size = wstr.size();
+
+	int mbcs_size = WideCharToMultiByte(
+		CP_UTF8,
+		0,
+		(wchar_t*)wstr.data(),
+		wcs_size >= INT_MAX ? throw std::invalid_argument("wstring is too long") : (int)wcs_size,
+		nullptr, 0, nullptr, nullptr
+	);
+
+	std::string mbcs{ (size_t)mbcs_size, '\0', std::allocator<char>{} };
+
+	WideCharToMultiByte(
+		CP_UTF8,
+		0,
+		(wchar_t*)wstr.data(), (int)wcs_size,
+		mbcs.data(), mbcs_size,
+		nullptr, nullptr
+	);
+
+	return mbcs;
 #else
 	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
 	return convert.to_bytes(str.data(), str.data() + str.size());
@@ -139,53 +304,17 @@ std::string EncodingConversion::UTF16toUTF8(const std::u16string& str)
 
 std::u32string EncodingConversion::UTF16toUTF32(const std::u16string& str)
 {
-	std::string bytes;
-	bytes.reserve(str.size() * 2);
-
-	for (const char16_t ch : str)
-	{
-		bytes.push_back((uint8_t)(ch / 256));
-		bytes.push_back((uint8_t)(ch % 256));
-	}
-
-#if defined(_MSC_VER)
-	std::wstring_convert<std::codecvt_utf16<uint32_t>, uint32_t> convert;
-	auto tmp = convert.from_bytes(bytes);
-	return std::u32string(tmp.data(), tmp.data() + tmp.size());
-#else
-	std::wstring_convert<std::codecvt_utf16<char32_t>, char32_t> convert;
-	return convert.from_bytes(bytes);
-#endif
+	throw std::logic_error("not implemented");
 }
 
 std::string EncodingConversion::UTF32toUTF8(const std::u32string& str)
 {
-#if defined(_MSC_VER)
-	std::wstring_convert<std::codecvt_utf8<uint32_t>, uint32_t> convert;
-	return convert.to_bytes((uint32_t*)str.data(), (uint32_t*)str.data() + str.size());
-#else
-	std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> convert;
-	return convert.to_bytes(str.data(), str.data() + str.size());
-#endif
+	throw std::logic_error("not implemented");
 }
 
 std::u16string EncodingConversion::UTF32toUTF16(const std::u32string& str)
 {
-#if defined(_MSC_VER)
-	std::wstring_convert<std::codecvt_utf16<uint32_t>, uint32_t> convert;
-	std::string bytes = convert.to_bytes((uint32_t*)str.data(), (uint32_t*)str.data() + str.size());
-#else
-	std::wstring_convert<std::codecvt_utf16<char32_t>, char32_t> convert;
-	std::string bytes = convert.to_bytes(str.data(), str.data() + str.size());
-#endif
-
-	std::u16string result;
-	result.reserve(bytes.size() / 2);
-
-	for (size_t i = 0; i < bytes.size(); i += 2)
-		result.push_back((char16_t)((uint8_t)(bytes[i]) * 256 + (uint8_t)(bytes[i + 1])));
-
-	return result;
+	throw std::logic_error("not implemented");
 }
 
 
